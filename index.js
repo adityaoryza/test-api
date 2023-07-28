932a5f7a58fbfdba0c5fe4ccf8c205a0eabc43d6
adityaoryza <oryza429@gmail.com>
2023-07-28 16:51:48 +0700

    add vercel json

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req, res) => {
-  res.json({
-    message: 'hello world',
-  });
+app.get('/', (req, res) => {
+  res.send('hello world');
 });
 
 const port = 3030;
